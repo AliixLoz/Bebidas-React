a53a7e4c66e0b764d8ff2cfeb930985874351596
Aliix <loveitza50@gmail.com>
2025-02-04 11:39:15 -0600

    02 implementa navegación con NavLink

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import IndexPage from "./views/IndexPage"
 import FavoritosPages from "./views/FavoritosPages"
+import Layout from "./layouts/Layout"
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<IndexPage/>} />
-                <Route path="favoritos" element={<FavoritosPages/>} />
+                <Route element={<Layout />}>
+                    <Route path="/" element={<IndexPage />} />
+                    <Route path="favoritos" element={<FavoritosPages />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
