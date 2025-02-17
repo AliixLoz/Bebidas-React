92de61ab4fb5d7e440b608abd9fdaac9ffa958bf
Aliix <loveitza50@gmail.com>
2025-02-17 09:19:11 -0600

    09 impements selectedRecipe in global state

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,20 +1,28 @@
 import axios from "axios"
-import { CategoriesAPIResponseSchema, RecipesAPIResponseSchema } from "../utils/recipe-schema"
-import { SearchFilters } from "../types"
+import { CategoriesAPIResponseSchema, RecipeAPIResponseSchema, RecipesAPIResponseSchema } from "../utils/recipe-schema"
+import { Drink, SearchFilters } from "../types"
 
 export async function getCategories() {
     const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list'
-    const {data} = await axios(url)
+    const { data } = await axios(url)
     const result = CategoriesAPIResponseSchema.safeParse(data)
-    if(result.success){
+    if (result.success) {
         return result.data
     }
 }
 
 export async function getRecipes(filters: SearchFilters) {
     const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filters.category}&i=${filters.ingredient}`
-    const {data} = await axios(url)
+    const { data } = await axios(url)
     const result = RecipesAPIResponseSchema.safeParse(data)
-    if(result.success)
+    if (result.success)
+        return result.data
+}
+
+export async function getRecipeById(id: Drink['idDrink']) {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    const { data } = await axios(url)
+    const result = RecipeAPIResponseSchema.safeParse(data.drinks[0])
+    if (result.success)
         return result.data
 }
